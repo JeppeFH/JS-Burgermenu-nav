13e5d70320768cd13c6ee11c7335aeecdb23df7f
JeppeFH <jeppefinch@hotmail.dk>
2024-05-27 14:42:37 +0200

    update

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/*Navigation*/
+let burgerMenuOpen = document.querySelector(".burgerMenuOpen");
+let burgerMenuClose = document.querySelector(".burgerMenuClose");
+let navLinks = document.querySelector(".nav-links");
+
+burgerMenuOpen.addEventListener("click", () => {
+  navLinks.classList.add("active");
+  burgerMenuOpen.classList.add("active");
+  burgerMenuClose.classList.add("active");
+});
+
+burgerMenuClose.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  burgerMenuOpen.classList.add();
+  burgerMenuClose.classList.remove("active");
+});
